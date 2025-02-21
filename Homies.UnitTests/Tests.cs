a7adb7f1433e20f33e4dcfd73885b14993098076
vilianakovacheva <vilianakovacheva04@gmail.com>
2025-02-21 10:39:56 +0200

    error commit

--- Homies.UnitTests/Tests.cs
+++ Homies.UnitTests/Tests.cs
@@ -32,7 +32,7 @@ namespace Homies.Tests.Controllers
 
             _context = new HomiesDbContext(options);
 
-            // Seed data
+            // Seed data test
             SeedDatabase();
 
             _controller = new EventController(_context);
